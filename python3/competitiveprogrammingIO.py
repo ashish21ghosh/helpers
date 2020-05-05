53045f7ae0ec581a368ad317941cf59ac7ba0fd4
Ashish Ghosh <ashish21ghosh@gmail.com>
2020-05-05 20:17:38 +0530

    IO

---
+++ python3/competitiveprogrammingIO.py
@@ -0,0 +1,9 @@
+# Basic Input/Output
+
+# Input: a line of input from STDIN 
+# Output: string
+input_data = input()
+
+# Input: space seperated list of numbers
+# Output: an array of integers
+input_arr = map(int, input().split())
